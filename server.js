95bf9e9ee95100d24c821e3184ffaae58d04098e
Chathura Prasanga <chathuraprasanga98@gmail.com>
2024-01-12 14:18:53 +0530

    removing date calculations

--- server.js
+++ server.js
@@ -1,56 +1,39 @@
-//Developer one- kaveesha
-
-//to establish a server
 const express = require('express') //call express
 const axios = require('axios') //call axos
-// Developer three Chathura
-const moment = require('moment') // call moment 
 
 const app = express()
 const PORT = 3000
 const usdUrl = 'https://www.cbsl.gov.lk/cbsl_custom/charts/usd/oneweek.php'
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
-
-//Developer two Isuru
 //Create an API to fetch usdUrl data
-app.get('/exchangeRate',async(req,res)=>{
-  // error handling
-    //dev 3
-  try{
-  const response = await axios.get(usdUrl);
-  const apiData =  response.data;
-  const lines = apiData.split('\n');
+app.get('/exchangeRate', async (req, res) => {
 
-  const nonEmptyLines = lines.filter(line => line.trim() !== '');
+  try {
+    const response = await axios.get(usdUrl);
+    const apiData = response.data;
+    const lines = apiData.split('\n');
 
-  const today = moment().format('YYYY-MM-DD');
-  const yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
+    const nonEmptyLines = lines.filter(line => line.trim() !== '');
 
+    const rates = nonEmptyLines.map(line => {
+      const [date, rate] = line.split('\t');
+      return { date, rate: parseFloat(rate).toFixed(1)};
+    });
 
-  const rates = nonEmptyLines.map(line => {
-    const [date, rate] = line.split('\t');
-    return {date, rate: parseFloat(rate).toFixed(1) };
-  });
+    const jsonData = {
+      currency: "USD/LKR",
+      rates: rates,
+    };
 
-  // take only rates today and yesterday
-  // dev 3
-  const filteredRates = rates.filter(rate => rate.date === today || rate.date === yesterday);
+    res.status(200).json(jsonData);
 
-  const jsonData = {
-    currency: "USD/LKR",
-    rates: filteredRates,
-  };
+  } catch (error) {
 
-  // console.log(jsonData)
-  res.status(200).json(jsonData);
-
-  // res.json(apiData);
-  }catch(error){
-    // console.log(error)
-    res.status(500).json({'message':'Internal Server Error'});
+    res.status(500).json({ 'message': 'Internal Server Error' });
   }
 });
 
+
+app.listen(PORT, () => {
+  console.log(`Server is running on: http://localhost:${PORT}`)
+})
