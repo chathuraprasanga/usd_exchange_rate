3c6d6d62bf8129561e6cadf55823d66899187d09
Isuru-Sandakalum <isurusandakalum99@gmail.com>
2024-01-12 11:02:34 +0530

    Developer two create API

--- server.js
+++ server.js
@@ -11,3 +11,13 @@ const usdUrl = 'https://www.cbsl.gov.lk/cbsl_custom/charts/usd/oneweek.php'
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
+//Developer two Isuru
+//Create an API to fetch usdUrl data
+app.get('/exchangeRate',async(req,res)=>{
+  const response = await axios.get(usdUrl);
+  const apiData =  response.data;
+
+  res.json(apiData);
+});
+
